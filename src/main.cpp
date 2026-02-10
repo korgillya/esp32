c760f57d783540209792bf1f26c46f334fa342b7
ikorg <ikorg@newagesol.co>
2026-02-10 22:02:01 +0100

    light system

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,102 @@
 #include <Arduino.h>
 
-#define CONTROL_LED_PIN_BLUE 14
-#define CONTROL_LED_PIN_YELL 13
-#define CONTROL_LED_PIN_GREEN 12
-#define CONTROL_LED_PIN_RED 11
+#define BUTTON_INPUT 13
+#define PHOTORESISTOR_INPUT 12
+#define CONTROL_LED_PIN_GREEN 14
+#define POTENTIOMETER_INPUT 11
+#define SERVO_OUTPUT 10
 
-// put function declarations here:
-int myFunction(int, int);
+#define U_REFERENCE 3.1
+#define MAX_DIGITAL 4095.0
+#define THRESHOLD_VOLTAGE 1.1
 
-void setup()
-{
-  // put your setup code here, to run once:
-  // int result = myFunction(2, 3);
+const int freq = 5000;    // Частота 5 кГц
+const int ledChannel = 0; // Канал ШІМ
+const int resolution = 8; // Роздільна здатність 8 біт (0-255)
+
+int currentMode = 0;        // Поточний режим
+const int maxModes = 2;     // Кількість режимів (наприклад, 0, 1)
+
+bool lastButtonState = LOW; // Попередній стан кнопки (HIGH через INPUT_PULLUP)
+
+
+void applyMode(int, int, float, int);
+
+void setup() {
   Serial.begin(115200);
-
-  pinMode(CONTROL_LED_PIN_BLUE, OUTPUT);
-  pinMode(CONTROL_LED_PIN_YELL, OUTPUT);
+  pinMode(BUTTON_INPUT, INPUT); // Використовуємо вбудовану підтяжку
   pinMode(CONTROL_LED_PIN_GREEN, OUTPUT);
-  pinMode(CONTROL_LED_PIN_RED, OUTPUT);
+  pinMode(POTENTIOMETER_INPUT, INPUT);
+
+  analogSetAttenuation(ADC_11db);
+  Serial.println("Система готова. Режим: 0");
 }
 
-void loop()
-{
+void loop() {
+ bool reading = digitalRead(BUTTON_INPUT);
 
-  //array of leds
-  uint16_t pins[] = {CONTROL_LED_PIN_BLUE, CONTROL_LED_PIN_YELL, CONTROL_LED_PIN_GREEN, CONTROL_LED_PIN_RED};
+  int rawValue = analogRead(PHOTORESISTOR_INPUT);
 
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(pins[i], HIGH); // Turn the LED on
-    delay(500); // Wait for a half of second
-    Serial.println("LED off");
-    digitalWrite(pins[i], LOW); // Turn the LED off
-    delay(500); // Wait for another half of second            
+  int potValue = analogRead(POTENTIOMETER_INPUT); // 0 - 4095
+
+  // Convert raw value to voltage (approx)
+  float voltage = (rawValue / MAX_DIGITAL) * U_REFERENCE; 
+
+  // Перевіряємо перехід з HIGH (натиснуто) в LOW (відпущено)
+  if (lastButtonState == HIGH && reading == LOW) {
+    delay(50); // Простий антибрязкіт
+    
+    currentMode++;
+    if (currentMode > 1) currentMode = 0; // Наприклад, 3 режими
+    
+    Serial.print("Кнопку відпущено! Поточний режим: ");
+    Serial.println(currentMode);
   }
 
+  applyMode(currentMode, rawValue, voltage, potValue);
+
+  // Запам'ятовуємо стан для наступного циклу
+  lastButtonState = reading;
 }
 
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+// void handleMode(int photoResistorInput, float voltage) {
+  
+// }
+
+void applyMode(int mode, int photoResistorInput, float voltage, int potValue) {
+  switch(mode) {
+    case 0: { 
+
+        Serial.println(photoResistorInput);
+        int onTimePhotoresistor = map(photoResistorInput, 500, 2200, 0, 10000); 
+        int offTimePhotoresistor = 10000 - onTimePhotoresistor;
+
+        if (onTimePhotoresistor > 0) {
+            digitalWrite(CONTROL_LED_PIN_GREEN, HIGH);
+            delayMicroseconds(onTimePhotoresistor);
+        }
+
+        if (offTimePhotoresistor > 0) {
+            digitalWrite(CONTROL_LED_PIN_GREEN, LOW);
+            delayMicroseconds(offTimePhotoresistor);
+        };
+      break;
+    }
+    case 1: 
+      // Перетворюємо значення в мікросекунди для періоду в 10мс (100 Гц)
+      // 10000 мкс - це повний цикл. 
+      int onTime = map(potValue, 0, 4095, 0, 10000); 
+      int offTime = 10000 - onTime;
+
+      if (onTime > 0) {
+          digitalWrite(CONTROL_LED_PIN_GREEN, HIGH);
+          delayMicroseconds(onTime);
+      }
+
+      if (offTime > 0) {
+          digitalWrite(CONTROL_LED_PIN_GREEN, LOW);
+          delayMicroseconds(offTime);
+      }
+     break;
+  }
 }
