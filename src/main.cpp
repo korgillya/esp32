77526d13eda3c96b1b35270736fa97747a1df9d1
ikorg <ikorg@newagesol.co>
2026-02-17 21:59:03 +0100

    improved code

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,46 @@
 #include <Arduino.h>
-
-#define BUTTON_INPUT 13
-#define PHOTORESISTOR_INPUT 12
-#define CONTROL_LED_PIN_GREEN 14
-#define POTENTIOMETER_INPUT 11
-#define SERVO_OUTPUT 10
-
-#define U_REFERENCE 3.1
-#define MAX_DIGITAL 4095.0
-#define THRESHOLD_VOLTAGE 1.1
-
-const int freq = 5000;    // Частота 5 кГц
-const int ledChannel = 0; // Канал ШІМ
-const int resolution = 8; // Роздільна здатність 8 біт (0-255)
-
-int currentMode = 0;        // Поточний режим
-const int maxModes = 2;     // Кількість режимів (наприклад, 0, 1)
-
-bool lastButtonState = LOW; // Попередній стан кнопки (HIGH через INPUT_PULLUP)
+#include <ESP32Servo.h>
+#include "esp_cpu.h"
 
 
-void applyMode(int, int, float, int);
+constexpr uint8_t BUTTON_INPUT =  13;
+constexpr uint8_t PHOTORESISTOR_INPUT = 12;
+constexpr uint8_t CONTROL_LED_PIN_GREEN = 14;
+constexpr uint8_t POTENTIOMETER_INPUT = 11;
+constexpr uint8_t SERVO_OUTPUT = 10;
+
+static const float U_REFERENCE = 3.1;
+static const float MAX_DIGITAL = 4095.0;
+
+enum class CurrentMode { PHOTORESISTOR, POTENTIOMETER, SERVO };
+
+void applyMode(CurrentMode mode, int photoInput, float voltage, int potValue, Servo &servo);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BUTTON_INPUT, INPUT); // Використовуємо вбудовану підтяжку
+  pinMode(BUTTON_INPUT, INPUT);
   pinMode(CONTROL_LED_PIN_GREEN, OUTPUT);
   pinMode(POTENTIOMETER_INPUT, INPUT);
-
   analogSetAttenuation(ADC_11db);
-  Serial.println("Система готова. Режим: 0");
 }
 
 void loop() {
- bool reading = digitalRead(BUTTON_INPUT);
+  //moved global variables to local
+  static uint32_t start_cycles;
+  static uint32_t end_cycles;
+  static CurrentMode currentMode = CurrentMode::PHOTORESISTOR;
+  static bool lastButtonState = LOW;
+
+  static Servo myServo;
+  static bool isServoInitialized = false;
+  if(!isServoInitialized) {
+    myServo.attach(SERVO_OUTPUT);
+    isServoInitialized = true;
+  }
+
+  start_cycles = esp_cpu_get_ccount();
+
+  bool reading = digitalRead(BUTTON_INPUT);
 
   int rawValue = analogRead(PHOTORESISTOR_INPUT);
 
@@ -42,32 +49,27 @@ void loop() {
   // Convert raw value to voltage (approx)
   float voltage = (rawValue / MAX_DIGITAL) * U_REFERENCE; 
 
-  // Перевіряємо перехід з HIGH (натиснуто) в LOW (відпущено)
   if (lastButtonState == HIGH && reading == LOW) {
-    delay(50); // Простий антибрязкіт
-    
-    currentMode++;
-    if (currentMode > 1) currentMode = 0; // Наприклад, 3 режими
-    
-    Serial.print("Кнопку відпущено! Поточний режим: ");
-    Serial.println(currentMode);
+    int nextMode = static_cast<int>(currentMode) + 1;
+    if (nextMode > static_cast<int>(CurrentMode::SERVO)) nextMode = 0;
+    currentMode = static_cast<CurrentMode>(nextMode);
+    Serial.println(nextMode);
   }
 
-  applyMode(currentMode, rawValue, voltage, potValue);
+  applyMode(currentMode, rawValue, voltage, potValue, myServo);
 
-  // Запам'ятовуємо стан для наступного циклу
   lastButtonState = reading;
+
+  end_cycles = esp_cpu_get_ccount();
+  uint32_t spent_cycles = end_cycles - start_cycles;
+  float seconds = spent_cycles / (ESP.getCpuFreqMHz() * 1000000.0);
+
+  Serial.printf("Cycles: %u | Freq: %u MHz | Core: %d | Time spent: %.9f s\n", spent_cycles, ESP.getCpuFreqMHz(), xPortGetCoreID(), seconds);
 }
 
-// void handleMode(int photoResistorInput, float voltage) {
-  
-// }
-
-void applyMode(int mode, int photoResistorInput, float voltage, int potValue) {
+void applyMode(CurrentMode mode, int photoResistorInput, float voltage, int potValue, Servo &servo) {
   switch(mode) {
-    case 0: { 
-
-        Serial.println(photoResistorInput);
+    case CurrentMode::PHOTORESISTOR: { 
         int onTimePhotoresistor = map(photoResistorInput, 500, 2200, 0, 10000); 
         int offTimePhotoresistor = 10000 - onTimePhotoresistor;
 
@@ -75,28 +77,29 @@ void applyMode(int mode, int photoResistorInput, float voltage, int potValue) {
             digitalWrite(CONTROL_LED_PIN_GREEN, HIGH);
             delayMicroseconds(onTimePhotoresistor);
         }
-
         if (offTimePhotoresistor > 0) {
             digitalWrite(CONTROL_LED_PIN_GREEN, LOW);
             delayMicroseconds(offTimePhotoresistor);
         };
       break;
     }
-    case 1: 
-      // Перетворюємо значення в мікросекунди для періоду в 10мс (100 Гц)
-      // 10000 мкс - це повний цикл. 
+    case CurrentMode::POTENTIOMETER: {
       int onTime = map(potValue, 0, 4095, 0, 10000); 
       int offTime = 10000 - onTime;
-
       if (onTime > 0) {
           digitalWrite(CONTROL_LED_PIN_GREEN, HIGH);
           delayMicroseconds(onTime);
       }
-
       if (offTime > 0) {
           digitalWrite(CONTROL_LED_PIN_GREEN, LOW);
           delayMicroseconds(offTime);
       }
      break;
+    }
+    case CurrentMode::SERVO: {
+      int angle = map(potValue, 0, 4095, 0, 180);
+      Serial.println(angle);
+      servo.write(angle);
+    }
   }
 }
